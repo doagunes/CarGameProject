d7ccc5b0831076f8c65c86cb8d4f3ff4313b7299
Doğa Güneş <120034878+doagunes@users.noreply.github.com>
2023-09-24 20:48:34 +0300

    some methods added

--- Assets/Collision.cs
+++ Assets/Collision.cs
@@ -4,17 +4,29 @@ using UnityEngine;
 
 public class Collision : MonoBehaviour
 {
-    float counter = 0.0f;
+    bool hasPacked; // değer atamamak false olarak başlatmaktır!!
+    bool hasDelivered;
+    
     
     void OnCollisionEnter2D(Collision2D other){
-        Debug.Log("Ohh..It Hurtsssss...!!!");
+
+       // Debug.Log("Ohh..It Hurtsssss...!!!");
         
     }
 
     
    void OnTriggerEnter2D(Collider2D other) {
-        counter++;
-        Debug.Log("You Got Coinnnn!!!");
+        
+        if(other.tag == "Package" && !hasPacked){
+            hasPacked = true;
+            hasDelivered = false;
+            Debug.Log("Package picked up!!!");
+            Destroy(other.gameObject);
+        }else if(other.tag == "Customer" && hasPacked && !hasDelivered){
+            Debug.Log("Customer: Thanksss..");
+            hasPacked = false;
+            hasDelivered = true;
+        }
        
     }
 
